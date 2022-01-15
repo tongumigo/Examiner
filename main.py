ddc212d855f484bed5e3b9ad85d22a7c522cb3a8
Tongu <tongumigo@gmail.com>
2022-01-15 23:30:10 +0200

    First Working Screenshort The generator function is working correctly returning a list of string examination numbers

---
+++ main.py
@@ -0,0 +1,39 @@
+def generator(exam_no,rang, slice_pos):
+    """ 
+    *******************************************************
+    |+ rang is the value to which values will be generated
+    |+ exam_no is the full examination number
+    |+ Slice_pos is the positional places from the end at
+    |+ which the new values are added 
+    ********************************************************
+    """
+
+
+    exam_num =  ""
+    exam_num = exam_no [:-2] 
+
+    for i in range(0, num):
+        exam_num = 
+        
+
+
+
+
+
+def messager():
+    pass
+
+
+
+def sender():
+    pass
+
+
+def formatter():
+    pass
+
+
+
+
+def display():
+    pass
